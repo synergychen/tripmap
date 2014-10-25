da27963ac6c088b3d3cbfdc2d10bfa9481c7434f
Chen Huang <alan.tolearn@gmail.com>
2014-10-25 13:54:36 -0400

    User can delete a trip

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root "trips#index"
 
-  resources :trips, only: [:index, :new, :create, :show, :edit, :update]
+  resources :trips, only: [:index, :new, :create, :show, :edit, :update, :destroy]
 end
